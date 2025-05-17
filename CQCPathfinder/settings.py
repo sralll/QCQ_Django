205af0e3b2f4144e044a5615cbf28f026f2edffb
Lars <larsbeglinger@gmail.com>
2025-05-17 22:35:59 +0200

    added requirements.txt to deploy on render for the first time

--- CQCPathfinder/settings.py
+++ CQCPathfinder/settings.py
@@ -31,6 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['qcq-django.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://qcq-django.onrender.com',
+]
 
 # Application definition
 
